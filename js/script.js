f11690ef5fbec4f6ccc01e6bab35cb59e00b5a3a
pdrgmz <pdrgmz2022@gmail.com>
2023-05-11 01:12:32 -0300

    subida de archivos

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// alert(" hola Pedro como estas?")
+// confirm("desea continuar en la Pagina?")    
+function CalPrecioFinal(){
+
+    console.log("Ingreso a calcular precio"); // esto es para debug por consola
+    // cantEnt almacena la cantidad de entradas que ingreso el usuario
+    let cantEnt=document.getElementById("cantidadentradas").value;
+        // console.log() me da los valores que va entregando la variable en la consola para corregir y lo quiero
+        console.log("cantEnt: " + cantEnt)
+
+
+    // porcDesc almacena el valor de descuento elegido por el usuario
+    let porcDesc=document.getElementById("porcDescuento").value;
+        console.log("porcDesc: " + porcDesc)
+
+
+    // descuento almacena los pesos totales a descontar
+    let descuento = (cantEnt*1000) * porcDesc/100;
+         console.log("descuento: " + descuento)
+
+
+    let precioTotal = (cantEnt*1000) - descuento;
+    // estoy modificando el html dinamicamente para colocar el precio total en la pantalla
+    document.getElementById("valorTotal").value=precioTotal;
+
+
+
+
+}
